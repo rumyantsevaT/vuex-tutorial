9effef71dacdc2d1bfebc9f11ffdf9f391ea3c79
Tina <ipochinili@gmail.com>
2021-04-23 22:48:23 +0300

    created components

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+<input type="text">
+<input type="submit">
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/TodoLIst.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Todo/>
+  </div>
+</template>
+
+<script>
+import Todo from './Todo.vue';
+
+  export default {
+    components: {
+      Todo,
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
